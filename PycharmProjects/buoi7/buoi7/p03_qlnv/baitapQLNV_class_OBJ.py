50c9e4b9423afe41b412949d14b3f615e1acd407
Thitrongdan202 <93548856+Thitrongdan202@users.noreply.github.com>
2024-08-04 09:56:20 +0700

    Update baitapQLNV_class_OBJ.py

--- PycharmProjects/buoi7/buoi7/p03_qlnv/baitapQLNV_class_OBJ.py
+++ PycharmProjects/buoi7/buoi7/p03_qlnv/baitapQLNV_class_OBJ.py
@@ -1,104 +1,98 @@
-# Lớp cơ sở NhanVien
-class NhanVien:
-    def __init__(self, ma_nv, ho_ten, luong_cb):
+class NVVP:
+    def __init__(self, ma_nv, **kwargs):
         self.ma_nv = ma_nv
-        self.ho_ten = ho_ten
-        self.luong_cb = luong_cb
-        self.luong_ht = 0  # Lương hằng tháng
+        self.ho_ten = kwargs.get('ho_ten', 'Cập nhật tên sau')
+        self.luong_cb = kwargs.get('luong_cb', 0)
+        self.so_ng = kwargs.get('so_ng', 0)
+        self.luong_ht = 0
 
     def __str__(self):
-        return f"Mã NV: {self.ma_nv}, Họ tên: {self.ho_ten}, Lương cơ bản: {self.luong_cb}, Lương hằng tháng: {self.luong_ht}"
+        return str([self.ma_nv, self.ho_ten, self.luong_cb, self.luong_ht, self.so_ng])
 
-
-# Lớp NhanVienVanPhong kế thừa từ NhanVien
-class NhanVienVanPhong(NhanVien):
-    def __init__(self, ma_nv, ho_ten, luong_cb, so_ng):
-        super().__init__(ma_nv, ho_ten, luong_cb)
-        self.so_ng = so_ng  # Số ngày làm việc
-
-    # Tính lương hằng tháng
     def tinh_luong_ht(self):
-        self.luong_ht = self.luong_cb + self.so_ng * 150_000
+        luong = self.luong_cb + self.so_ng * 150_000
+        self.luong_ht = luong
+        return luong
 
 
-# Lớp NhanVienBanHang kế thừa từ NhanVien
-class NhanVienBanHang(NhanVien):
-    def __init__(self, ma_nv, ho_ten, luong_cb, so_sp):
-        super().__init__(ma_nv, ho_ten, luong_cb)
-        self.so_sp = so_sp  # Số sản phẩm bán được
+class NVBH:
+    def __init__(self, ma_nv, **kwargs):
+        self.ma_nv = ma_nv
+        self.ho_ten = kwargs.get('ho_ten', 'Cập nhật tên sau')
+        self.luong_cb = kwargs.get('luong_cb', 0)
+        self.so_sp = kwargs.get('so_sp', 0)
+        self.luong_ht = 0
 
-    # Tính lương hằng tháng
+    def __str__(self):
+        return str([self.ma_nv, self.ho_ten, self.luong_cb, self.luong_ht, self.so_sp])
+
     def tinh_luong_ht(self):
-        self.luong_ht = self.luong_cb + self.so_sp * 18_000
+        luong = self.luong_cb + self.so_sp * 18_000
+
+        self.luong_ht = luong
+        return luong
 
 
-# Lớp CongTy quản lý danh sách nhân viên
 class CongTy:
-    def __init__(self):
-        self.dsNV = []  # Danh sách nhân viên
+    def __init__(self, ma_ct, ten_ct):
+        self.ma_ct = ma_ct
+        self.ten_ct = ten_ct
+        self.ds = []
+
 
     # Câu 1: Khởi tạo tự động các nhân viên
-    def init_data_nv(self):
-        nv1 = NhanVienVanPhong(1, "Nguyễn Văn A", 7_000_000, 22)
-        nv2 = NhanVienBanHang(2, "Trần Thị B", 8_000_000, 120)
-        self.dsNV.extend([nv1, nv2])
+    def init_ds_nv(self):
+        nv0 = NVBH(122, luong_cb=1_000_000)
+        nv1 = NVVP(123, ho_ten="Nguyễn Văn A", luong_cb=9_000_000, so_ng=20)
+        nv2 = NVBH(124, ho_ten="Nguyễn Văn B", luong_cb=6_000_000, so_ng=90)
+        nv3 = NVVP(125, ho_ten="Nguyễn Văn C", luong_cb=7_000_000, so_ng=23)
+
+        self.ds.extend([nv1, nv2, nv3, nv0])
+
 
     # Câu 2: Xuất các nhân viên trong công ty
-    def print_ds_nv(self):
-        for nv in self.dsNV:
-            print(nv)
-
-    # Câu 3: Tính lương hằng tháng của các nhân viên trong công ty
     def tinh_luong_ht(self):
-        for nv in self.dsNV:
+        for nv in self.ds:
             nv.tinh_luong_ht()
 
+    # Câu 3: Tính lương hằng tháng của các nhân viên trong công ty
+    def print_ds_nv(self):
+        for nv in self.ds:
+            print(nv)
+
     # Câu 4: Tìm nhân viên theo mã nhân viên
-    def tim_nv_manv(self, ma_nv):
-        for nv in self.dsNV:
-            if nv.ma_nv == ma_nv:
+    def tim_nv_manv(self, manv):
+        for nv in self.ds:
+            if manv == nv.ma_nv:
                 return nv
-        return None
 
     # Câu 5: Tìm nhân viên có lương hằng tháng cao nhất
-    def tim_nv_luong_cao_nhat(self):
-        if not self.dsNV:
-            return None
-        return max(self.dsNV, key=lambda nv: nv.luong_ht)
 
     # Câu 6: Tìm nhân viên bán hàng có lương hằng tháng thấp nhất
-    def tim_nv_bh_luong_thap_nhat(self):
-        nv_bh = [nv for nv in self.dsNV if isinstance(nv, NhanVienBanHang)]
-        if not nv_bh:
-            return None
-        return min(nv_bh, key=lambda nv: nv.luong_ht)
+
 
 
 if __name__ == '__main__':
     # Tạo đối tượng công ty
-    ct = CongTy()
+    ct = CongTy(369, "Tesla")
 
-    # Test: Câu 1: Tạo danh sách Nhân viên
-    ct.init_data_nv()
-
-    # Test: Câu 3: Tính lương nhân viên trong công ty
-    ct.tinh_luong_ht()
-
-    # Test: Câu 2: In danh sách nhân viên trong công ty
+    print("Test: 1. Khởi tạo ds nhân viên")
+    ct.init_ds_nv()
     ct.print_ds_nv()
 
-    # Test: Tìm nhân viên theo mã nhân viên
-    ma_nv_tim_kiem = 2
-    nv_tim_thay = ct.tim_nv_manv(ma_nv_tim_kiem)
-    if nv_tim_thay:
-        print(f"\nNhân viên có mã {ma_nv_tim_kiem}: {nv_tim_thay}")
+    print("Test: 2.Tính lương từng nhân viên")
+    ct.tinh_luong_ht()
 
-    # Test: Tìm nhân viên có lương hằng tháng cao nhất
-    nv_luong_cao_nhat = ct.tim_nv_luong_cao_nhat()
-    if nv_luong_cao_nhat:
-        print(f"\nNhân viên có lương hằng tháng cao nhất: {nv_luong_cao_nhat}")
+    print("\nTest: 3. In danh sách nhân viên trong công ty:")
+    ct.print_ds_nv()
 
-    # Test: Tìm nhân viên bán hàng có lương hằng tháng thấp nhất
-    nv_bh_luong_thap_nhat = ct.tim_nv_bh_luong_thap_nhat()
-    if nv_bh_luong_thap_nhat:
-        print(f"\nNhân viên bán hàng có lương hằng tháng thấp nhất: {nv_bh_luong_thap_nhat}")
+    print("\nTest: 4.Tìm nhân viên theo mã nhân viên")
+    nv = ct.tim_nv_manv(123)
+    print("\t Nhan viên: ", nv)
+    print("\t Nhan viên: ", ct.tim_nv_manv(124))
+
+    if ct.tim_nv_manv(125):
+        print("Tìm thấy")
+    else:
+        print("Không thấy")
+
