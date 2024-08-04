f0fd59e25c609b14baf0970af8f38f30cb765d32
Thitrongdan202 <93548856+Thitrongdan202@users.noreply.github.com>
2024-08-04 10:21:37 +0700

    Update QLNV_donggoi_kethua.py

--- PycharmProjects/buoi7/buoi7/p03_qlnv/P04/P07/QLNV_donggoi_kethua.py
+++ PycharmProjects/buoi7/buoi7/p03_qlnv/P04/P07/QLNV_donggoi_kethua.py
@@ -33,20 +33,48 @@ class NVBH(NhanVien):
         self.__so_sp = kwargs.get('so_sp, 0')
 
     def __str__(self):
-        return "NVBH: " + str([self._ma_nv, self._ho_ten, self._luong_cb, self.luong_ht, self.__so_sp])
+        return "NVBH: " + str([self._ma_nv, self._ho_ten,
+                               self._luong_cb, self._luong_ht, self.__so_sp])
 
     def tinh_luong_ht(self):
         pass
 class CongTy:
-    class CongTy:
         def __init__(self, ma_ct, ten_ct):
             self.__ma_ct = ma_ct
             self.__ten_ct = ten_ct
             self.__ds = []
 
+        # Câu 1: Khởi tạo tự động các nhân viên
+        def init_ds_nv(self):
+            nv0 = NVBH(122, luong_cb=1_000_000)
+            nv1 = NVVP(123, ho_ten="Nguyễn Văn A", luong_cb=9_000_000, so_ng=20)
+            nv2 = NVBH(124, ho_ten="Nguyễn Văn B", luong_cb=6_000_000, so_ng=90)
+            nv3 = NVVP(125, ho_ten="Nguyễn Văn C", luong_cb=7_000_000, so_ng=23)
 
+            self.__ds.extend([nv1, nv2, nv3, nv0])
 
+        # Câu 2: Xuất các nhân viên trong công ty
+        def tinh_luong_ht(self):
+            for nv in self.__ds:
+                nv.tinh_luong_ht()
+
+        # Câu 3: Tính lương hằng tháng của các nhân viên trong công ty
+        def print_ds_nv(self):
+            for nv in self.__ds:
+                print(nv)
 if __name__ == '__main__':
+    ct = CongTy(369, "Tesla")
+
+    print("Test: 1. Khởi tạo ds nhân viên")
+    ct.init_ds_nv()
+    ct.print_ds_nv()
+
+    print("Test: 2.Tính lương từng nhân viên")
+    ct.tinh_luong_ht()
+
+    print("\nTest: 3. In danh sách nhân viên trong công ty:")
+    ct.print_ds_nv()
+
     vp1 = NVVP(123, ho_ten="Nguyễn Văn A", luong_cb=1_000_000, so_ng=20)
     print(vp1)
 
